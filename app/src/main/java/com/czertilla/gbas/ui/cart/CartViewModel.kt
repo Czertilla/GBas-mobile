29fdd3a2dedc276d72e442710774e1dbada5213e
Czertilla <blaztermapskorsak@gmail.com>
2025-05-22 00:35:43 +0300

    feat(ui,res): add cart fragment

---
+++ app/src/main/java/com/czertilla/gbas/ui/cart/CartViewModel.kt
@@ -0,0 +1,13 @@
+package com.czertilla.gbas.ui.cart
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is favorite Fragment"
+    }
+    val text: LiveData<String> = _text
+}
