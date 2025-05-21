d28ae930faec80bc7caed984406e143af7276dbc
Czertilla <blaztermapskorsak@gmail.com>
2025-05-22 00:36:19 +0300

    feat(ui,res): add profile fragment

---
+++ app/src/main/java/com/czertilla/gbas/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,13 @@
+package com.czertilla.gbas.ui.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is favorite Fragment"
+    }
+    val text: LiveData<String> = _text
+}
