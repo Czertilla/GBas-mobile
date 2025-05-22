8505e58ec29229431a32f42dbbf9aed0e72770a2
Czertilla <blaztermapskorsak@gmail.com>
2025-05-22 13:27:02 +0300

    feat(src): add ActivityAuth

---
+++ app/src/main/java/com/czertilla/gbas/AuthActivity.kt
@@ -0,0 +1,26 @@
+package com.czertilla.gbas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.czertilla.gbas.ui.login.LoginFragment
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LoginFragment())
+                .commit()
+        }
+    }
+
+    fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
