8d494d42184abd39437e65451b4ed8f797ac62f2
Czertilla <blaztermapskorsak@gmail.com>
2025-05-22 21:34:58 +0300

    feat(local): add UserEntity

---
+++ app/src/main/java/com/czertilla/gbas/data/local/entity/UserEntity.kt
@@ -0,0 +1,13 @@
+package com.czertilla.gbas.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "user")
+data class UserEntity(
+    @PrimaryKey val id: UUID,
+    val displayName: String,
+    val email: String?,
+    val photoUrl: String?
+)
