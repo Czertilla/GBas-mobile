fdf351d51beeccbb1dde3785db946d16315cdf4c
Czertilla <blaztermapskorsak@gmail.com>
2025-05-22 21:35:44 +0300

    feat(enum): add UserDao

---
+++ app/src/main/java/com/czertilla/gbas/data/local/dao/UserDao.kt
@@ -0,0 +1,16 @@
+package com.czertilla.gbas.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.czertilla.gbas.data.local.entity.UserEntity
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user: UserEntity)
+
+    @Query("SELECT * FROM user WHERE id = :id")
+    suspend fun getById(id: String): UserEntity?
+}
